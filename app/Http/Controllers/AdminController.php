ccbd16650defa79544dd76e451a4a60941623689
Saheed Shittu <saheedsh2@gmail.com>
2025-01-20 23:00:54 +0100

    Created the Product Model and linked Category and Brand Tables

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -196,5 +196,18 @@ class AdminController extends Controller
 
     }
 
+    public function category_delete($id)
+    {
+        $category = Category::find($id);
+        if(File::exists(public_path('uploads/categories').'/'.$category->image))
+        {
+            File::delete(public_path('uploads/categories').'/'.$category->image);
+
+        }
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status','Category has been deleted Successfully');
+
+    }
+
   
 }

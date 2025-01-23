e22cc8855fc896602ffe355904f15ae9e35d4d3f
Saheed Shittu <saheedsh2@gmail.com>
2025-01-23 01:04:44 +0100

    Created Product-Add Page and uploads

--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,15 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::delete('/admin/brands/{id}/delete',[AdminController::class, 'brand_delete'])->name('admin.brand.delete');
     
     
-    Route::get('admin/categories',[AdminController::class, 'categories'])->name('admin.categories');
-    Route::get('admin/category/add',[AdminController::class, 'category_add'])->name('admin.category.add');
-    Route::post('admin/category/store',[AdminController::class, 'category_store'])->name('admin.category.store');
+    Route::get('/admin/categories',[AdminController::class, 'categories'])->name('admin.categories');
+    Route::get('/admin/category/add',[AdminController::class, 'category_add'])->name('admin.category.add');
+    Route::post('/admin/category/store',[AdminController::class, 'category_store'])->name('admin.category.store');
     Route::get('/admin/category/{id}/edit',[AdminController::class, 'category_edit'])->name('admin.category.edit');
     Route::put('/admin/category/update',[AdminController::class, 'category_update'])->name('admin.category.update');
     Route::delete('/admin/category/{id}/delete',[AdminController::class, 'category_delete'])->name('admin.category.delete');
+
+    Route::get('/admin/products',[AdminController::class,'products'])->name('admin.products');
+    Route::get('/admin/product/add', [AdminController::class, 'product_add'])->name('admin.product.add');
+    Route::post('/admin/product/store',[AdminController::class, 'product_store'])->name('admin.product.store');
 
 });
